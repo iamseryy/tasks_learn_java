33505deeec6a15f4e062a96bf3b4a2c624fd1138
desktop <psomail@yandex.ru>
2023-02-10 23:22:00 +0300

    task1 ready

--- task1/triangular_numbers_calculator/src/main/java/TriangularNumbersCalculator.java
+++ task1/triangular_numbers_calculator/src/main/java/TriangularNumbersCalculator.java
@@ -1,9 +1,49 @@
+import java.util.Scanner;
+
 public class TriangularNumbersCalculator {
     public static void main(String[] args){
 
+        try{
+            int number = getNumberFromUser("Enter a non-negative integer or empty line to cancel: ");
+            System.out.println("Result = " + String.valueOf(calculate(number)));
+        } catch (EmptyLineToCancelException e){
+            System.out.println("Canceled");
+        }
+    }
+
+    private static int getNumberFromUser(String mess) throws EmptyLineToCancelException{
+        Scanner input = new Scanner(System.in);
+        int number = -1;
+        do{
+            System.out.print(mess);
+            String line = input.nextLine();
+
+            if (line.isEmpty()){
+                throw new EmptyLineToCancelException("Canceled");
+            }
+
+            try{
+                number = Integer.parseInt(line);
+                if (number < 0){
+                    System.out.println("It's must be a non-negative integer");
+                }
+            } catch(Exception e){
+                System.out.println("Not an integer");
+            }
+        } while (number < 0);
+
+        input.close();
+
+        return number;
     }
 
     public static int calculate(int number){
         return number * (number + 1) / 2;
+    }
+
+    public static class EmptyLineToCancelException extends Exception {
+        public EmptyLineToCancelException (String message) {
+            super(message);
+        }
     }
 }
