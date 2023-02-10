81dca67a17fc3eacf69c4bc5ce0500a2422cf3a1
desktop <psomail@yandex.ru>
2023-02-10 21:13:23 +0300

    first commit

---
+++ task1/triangular_numbers_calculator/src/main/java/TriangularNumbersCalculator.java
@@ -0,0 +1,9 @@
+public class TriangularNumbersCalculator {
+    public static void main(String[] args){
+
+    }
+
+    public static int calculate(int number){
+        return number * (number + 1) / 2;
+    }
+}
